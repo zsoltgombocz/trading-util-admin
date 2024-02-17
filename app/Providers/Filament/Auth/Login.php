4459ef07b7a91391855e72395d05409aec197f4a
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2024-02-17 13:41:22 +0000

    ❇️ Add  username based login

---
+++ app/Providers/Filament/Auth/Login.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers\Filament\Auth;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\Login as BaseLogin;
+use Filament\Forms\Components\Component;
+
+class Login extends BaseLogin
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getNameFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getNameFormComponent(): Component
+    {
+        return TextInput::make('name')
+            ->label('Username')
+            ->required()
+            ->autocomplete()
+            ->autofocus();
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        return [
+            'name' => $data['name'],
+            'password' => $data['password'],
+        ];
+    }
+}
